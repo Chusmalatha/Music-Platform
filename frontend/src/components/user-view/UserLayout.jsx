0fe4fcdc90573ff2de9c2a8d5969442eb12f7764
Chusmalatha <196618446+Chusmalatha@users.noreply.github.com.>
2025-09-20 14:31:00 +0530

    Fifteen change

--- frontend/src/components/user-view/UserLayout.jsx
+++ frontend/src/components/user-view/UserLayout.jsx
@@ -6,7 +6,7 @@ const UserLayout = () => {
   return (
     <div className="min-h-screen overflow-hidden bg-gray-50 flex flex-col">
       <UserHeader />
-      <main className="pt-16 max-w-[100vw] overflow-hidden">
+      <main className="pt-16 ">
         <Outlet /> {/* Nested routes render here */}
       </main>
     </div>
--- frontend/src/pages/user-view/home.jsx
+++ frontend/src/pages/user-view/home.jsx
@@ -2,16 +2,17 @@ import React from "react";
 
 const UserHome = () => {
   return (
-    <div className="relative w-screen max-h-screen overflow-hidden flex items-center justify-center">
+    <div className="relative w-screen min-h-screen overflow-hidden flex items-center justify-center">
       {/* Background Image */}
       <img
         src="https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4"
         alt="Band performing music live"
-        className="absolute left-0 top-0  object-contain"
+        className="absolute inset-0 w-full h-full object-cover"  
+       
       />
 
       {/* Gradient Overlay */}
-      <div className="absolute inset-0 bg-gradient-to-b h-full  from-black/70 via-black/40 to-black/80" />
+      <div className="absolute inset-0 bg-gradient-to-b   from-black/70 via-black/40 to-black/80" />
 
       {/* Content */}
       <div className="relative  z-10 flex flex-col items-center text-center px-6 sm:px-12 lg:px-24 max-w-3xl">
